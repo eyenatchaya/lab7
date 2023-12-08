0513db6817ebd2f8de34d07922d329607e4d1af6
eye <eyenathaya5@gmail.com>
2023-12-08 16:52:58 +0700

    Filled out lab_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+
+	if(x >'A' && x<='Z'){
+		return x-1;
+	}else if(x=='A'){
+		return 'Z';
+	}
+	else 
+	{
+		return '0';
+	}
 }
 
 int main(){
-	//Test Case
+	char x;
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
